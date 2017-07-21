fa0c59e705d59eb0228eb954b9a89bca13fbcddc
Nicolas Schmidt <nfschmidt@gmail.com>
2017-07-21 20:12:11 -0300

    Finished solution for set01 challenge04

--- shell/set01-challenge04.sh
+++ shell/set01-challenge04.sh
@@ -2,9 +2,9 @@
 
 for input in $(cat ../files/set01-challenge04.txt)
 do
-    echo $(
     echo -n "$input" \
         | cryt decode hex \
         | cryt attack xor --criterion=text --detailed
-    )
-done
+done \
+    | sort -n -k4 \
+    | tail -n 1
